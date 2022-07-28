617105ffb0c4d73388184b983af1f9a4cc868d23
Emre Cetinkaya <emrecetinkayadev@gmail.com>
2022-07-28 10:33:02 +0300

    Test functions added 1

--- TypeScript/test/mocha/gilded-rose.spec.ts
+++ TypeScript/test/mocha/gilded-rose.spec.ts
@@ -1,10 +1,35 @@
 import { expect } from 'chai';
 import { Item, GildedRose } from '@/gilded-rose';
 
+const itemList = [
+  new Item("+5 Dexterity Vest", 10, 20), //
+  new Item("Aged Brie", 2, 0), //
+  new Item("Elixir of the Mongoose", 5, 7), //
+  new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+  new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+  new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+  new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+  new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+  // this conjured item does not work properly yet
+  new Item("Conjured Mana Cake", 3, 6)];
+
 describe('Gilded Rose', () => {
-  it('should foo', () => {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).to.equal('fixme');
+  const gildedRose = new GildedRose(itemList);
+  const items = gildedRose.updateQuality();
+
+  it('+5 Dexterity Vest', () => {
+    expect(items[0].sellIn, 'Sell Value').equal(9);
+    expect(items[0].quality, 'Quality').equal(19);
   });
+
+  it('Aged Brie', () => {
+    expect(items[1].sellIn, 'Sell Value').equal(1);
+    expect(items[1].quality, 'Quality').equal(-1);
+  });
+
+  it('Elixir of the Mongoose', () => {
+    expect(items[2].sellIn, 'Sell Value').equal(4);
+    expect(items[2].quality, 'Quality').equal(6);
+  });
+
 });
